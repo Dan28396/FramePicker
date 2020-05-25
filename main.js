0dc14a0eb7ad2701e6cbdec24ac42ad5eb174a3e
Dan28396 <lobchenko1999@gmail.com>
2020-05-26 01:45:04 +0300

    Major improvements and styles

--- main.js
+++ main.js
@@ -6,14 +6,24 @@ const httpServer = http.createServer(app);
 const WebSocket = require('ws');
 const fs = require('fs')
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3000;
 
+
 const wsServer = new WebSocket.Server({server: httpServer}, () => console.log(`WS server is listening at ws://localhost:${WS_PORT}`));
-let correctData, count = 1;
+let correctData, count = 0;
+
 wsServer.on('connection', (ws, req) => {
     console.log('Connected');
     fs.readdir(path.resolve(__dirname, `./photos`), (err, files) => {
-        count = files.length + 1
+        if (err) {
+            fs.mkdir(path.resolve(__dirname, 'photos'), (err) => {
+                if (err) {
+                    console.log("Photos directory wasn't created!")
+                } else console.log("Photos directory created!")
+            })
+        } else count = files.length + 1
     });
     ws.on('message', data => {
         correctData = data.split(';base64,').pop();
@@ -26,6 +36,7 @@ wsServer.on('connection', (ws, req) => {
 });
 
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
+
 
 httpServer.listen(PORT, () => console.log(`HTTP server listening at http://localhost:${PORT}`));
---
+++ public/picker.js
@@ -0,0 +1,57 @@
+const video = document.querySelector('video');
+const img = document.querySelector('img');
+const button = document.querySelector('button');
+const range = document.querySelector('input');
+const output = document.querySelector('.fps__counter');
+let interval;
+let FPS = 1;
+range.value = FPS;
+navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => video.srcObject = stream);
+let isCapturing = false;
+
+
+button.addEventListener('click', toggleCapture)
+range.addEventListener('change', changeFps)
+
+function getFrame() {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    const data = canvas.toDataURL('image/jpg', 1);
+    img.src = data;
+    return data
+}
+
+function changeFps() {
+    output.innerHTML = range.value;
+    FPS = Number(range.value)
+}
+
+changeFps()
+
+
+const WS_URL = location.origin.replace(/^http/, 'ws');
+const ws = new WebSocket(WS_URL);
+ws.onopen = () => {
+    console.log(`Connected to ${WS_URL}`);
+    setInterval(() => {
+        if (isCapturing) {
+            ws.send(getFrame());
+        }
+    }, 1000 / FPS);
+}
+
+function toggleCapture() {
+    isCapturing = !isCapturing;
+    button.innerHTML =
+        (button.innerHTML === 'Старт') ? button.innerHTML = 'Стоп' : button.innerHTML = 'Старт';
+    if (ws.readyState === WebSocket.OPEN) {
+        interval = setInterval(() => {
+            if (isCapturing) {
+                ws.send(getFrame());
+            }
+        }, 1000 / FPS);
+    } else console.log("Socket is closed!")
+    range.disabled = isCapturing === true;
+}
